cbb4a026b4bad6b5efbecc883f8c583bd29b8ba1
aofattporn <aofattapon321@gmail.com>
2023-07-16 02:06:15 +0700

    - change route name - add section input

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,13 @@ export default function Home() {
               Actually i don't know what i'll give to your gift. I think this
               web instead my gift and my wish to you.
               <h1 aria-label="Hi! I'm a developer">
-                wish you happy with &nbsp;<span className="typewriter thick"></span>
+                wish you happy with &nbsp;
+                <span className="typewriter thick"></span>
               </h1>
             </p>
           </div>
           <div className="mt-12">
-            <Link href={"/about"}>
+            <Link href={"/myWhish"}>
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
                 click to next page
               </button>
