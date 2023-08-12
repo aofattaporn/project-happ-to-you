275bef9b0688eac12deb4e29ff4f26a8914a44e7
aofattaporn <aofattapon321@gmail.com>
2023-08-12 13:27:54 +0700

    add env

--- app/components/galleryPhoto/CardPhoto.tsx
+++ app/components/galleryPhoto/CardPhoto.tsx
@@ -2,13 +2,14 @@ type CardPhotoProps = {
   srcImage: string;
   wish: string;
 };
+import { kanit_latin_200, kanit_latin_300 } from "@/app/types/fontsTypes";
 import { Kanit } from "next/font/google";
 
 export const kanit = Kanit({ subsets: ["latin"], weight: ["400"] });
 
 const CardPhoto = ({ srcImage, wish }: CardPhotoProps) => {
   return (
-    <li className={kanit.className}>
+    <li className={kanit_latin_300.className}>
       <div className="bg-white w-52 h-80 mr-32 card p-8 cursor-pointer ">
         <div className="photo h-3/4">
           <img className="object-contain h-48 w-96  " src={srcImage} />
